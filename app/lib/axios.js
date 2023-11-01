d209748fdc6ba4046ef07b098163633dff9545ca
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-11-01 20:44:31 +0700

    refactor : base url prod

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://ynrrizki-portfolio.vercel.app/api',
+    // baseURL: 'http://localhost:3000/api',
     // baseURL: 'https://8jx8wmjv-3000.asse.devtunnels.ms/api',
 });
